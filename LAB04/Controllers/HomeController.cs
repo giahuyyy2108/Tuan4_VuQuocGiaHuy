e40d3d89ae3af8e1620b4352ea0f5de22b7339bf
Gia Huy <giahuy1@unicorn365.net>
2022-03-16 11:48:22 +0700

    Add project files.

---
+++ LAB04/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LAB04.Models;
+using PagedList;
+
+namespace LAB04.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDataDataContextDataContext data = new MyDataDataContextDataContext();
+        public ActionResult Index(int ? page)
+        {
+            if (page == null) page = 1;
+            var all_sach = (from ss in data.Saches select ss).OrderBy(m=> m.masach);
+            int pageSize = 3;
+            int pageNum = page ?? 1;
+            return View(all_sach.ToPagedList(pageNum,pageSize));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
